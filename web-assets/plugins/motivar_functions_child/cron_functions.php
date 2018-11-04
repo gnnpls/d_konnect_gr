bddbefdbaf221855c2883b6364435638c4f711b6
gnnpls <n.giannopoulos@motivar.io>
2018-11-04 21:13:17 +0200

    fixes for collecting the payments

--- web-assets/plugins/motivar_functions_child/cron_functions.php
+++ web-assets/plugins/motivar_functions_child/cron_functions.php
@@ -1,14 +1,74 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) exit;
-/*
-if ( ! wp_next_scheduled( 'mycron_job' ) ) {
-  wp_schedule_event(time(),'daily', ',mycron_job');
-  }
-
-add_action('mycron_job','mycrons');
-
-
-function mycrons() {
-
+if (!defined('ABSPATH')) {
+    exit;
 }
-*/
+
+if (!wp_next_scheduled('konnect_collect_money')) {
+    wp_schedule_event(time(), 'daily', 'konnect_collect_money');
+}
+
+add_action('konnect_collect_money', 'konnect_collect_money_function');
+
+function konnect_collect_money_function()
+{
+
+    $month = 8;
+    $year = 2018;
+
+    $fields = array('month' => $month, 'year' => $year);
+
+    $data = konnect_rest_php('get', 'https://i.konnect.gr/wp-json/i-konnect-gr/konnect-acommodation-income', $fields);
+
+    if (!empty($data)) {
+
+        $args = array(
+            'post_type' => 'easy_finances',
+            'post_status' => 'publish',
+            'numberposts' => 1,
+            'fields' => 'ids',
+        );
+        $meta_array = array('relation' => 'AND');
+
+        $meta_array[] = array(
+            'key' => 'month',
+            'value' => array((int) $month, $month),
+            'compare' => 'IN',
+        );
+        $meta_array[] = array(
+            'key' => 'year',
+            'value' => (int) $year,
+            'compare' => '=',
+        );
+
+        $args['meta_query'] = $meta_array;
+
+        $posts = get_posts($args);
+
+        if (!empty($posts)) {
+            $id = $posts[0];
+
+            delete_field('income', $id);
+            $income = array();
+            $time = date('d/m/Y', strtotime('now'));
+            foreach ($data as $key => $dato) {
+              if ($dato['paid']>0)
+              {
+
+              
+                $new_value = array('date' => $time, 'type' => 17, 'source' => 20, 'amount' => $dato['paid'], 'final' => $dato['paid'], 'income_source' => $dato['title'], 'reason' => 'Konnect Fee - '.$dato['all']);
+                update_row('income', $key, $new_value, $id);
+                }
+                
+
+            }
+
+        }
+    }
+    return true;
+}
+
+add_action('admin_init', function () {
+    if (isset($_REQUEST['test_crons'])) {
+        konnect_collect_money_function();
+    }
+}, 10, 1);
--- web-assets/plugins/motivar_functions_child/custom_site_raw_code.php
+++ web-assets/plugins/motivar_functions_child/custom_site_raw_code.php
@@ -1,3 +1,61 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) exit;
 
+
+
+
+if (!function_exists('konnect_rest_php')) {
+    function konnect_rest_php($method, $url, $data = false, $json = true, $debug = false)
+    {
+
+        $result = array();
+        $curl = curl_init();
+
+        $data = apply_filters('konnect_rest_php_filter', $data, $url);
+        $method = strtolower($method);
+        switch ($method) {
+            case "post":
+                curl_setopt($curl, CURLOPT_POST, 1);
+                if ($data) {
+                    $data = urldecode(http_build_query($data));
+                    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+                }
+                break;
+            default:
+                if ($data) {
+                    $url = sprintf("%s?%s", $url, http_build_query($data));
+                }
+
+        }
+
+        // Optional Authentication:
+
+        curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($curl, CURLOPT_COOKIESESSION, true);
+        if ($debug) {
+            curl_setopt($curl, CURLOPT_VERBOSE, true);
+        }
+
+        $result = curl_exec($curl);
+        if ($result !== false) {
+            curl_close($curl);
+        }
+
+        if ($debug) {
+            print_r($result);
+            die();
+        }
+
+        if ($json) {
+            $result = json_decode($result, true);
+        }
+        if (isset($result['code'])) {
+            $result = array();
+        }
+        return $result;
+    }
+}
